1d2b8fb5f723a66ae99bce9b858ec1b7f4c527f1
Syver <syver.storm.furru@gmail.com>
2021-04-11 23:12:13 +0200

    Add preliminary branching

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,12 @@ struct AST {
 }
 
 #[derive(Debug)]
+enum Val {
+    Float(f64),
+    Bool(bool),
+}
+
+#[derive(Debug)]
 enum Token {
     Word(String),
     Number(f64),
@@ -28,7 +34,10 @@ impl Token {
 
         match word {
             "print" => Builtin(Print),
+            "branch" => Builtin(Branch),
+            "branch?" => Builtin(Cond),
             "+" => Builtin(Plus),
+            "=" => Builtin(Equal),
             "-" => Builtin(Minus),
             "*" => Builtin(Multiply),
             "/" => Builtin(Divide),
@@ -41,41 +50,96 @@ impl Token {
 enum Builtins {
     Print,
     Plus,
+    Equal,
     Minus,
     Multiply,
     Divide,
+    Branch,
+    Cond,
 }
 
 impl Builtins {
-    fn eval(&self, stack: &mut Vec<f64>) {
+    fn eval(&self, stack: &mut Vec<Val>) -> Option<usize> {
         use Builtins::*;
         match self {
             Print => {
                 if let Some((last, _)) = stack.split_last() {
-                    println!("{}", last);
+                    println!("{:?}", last);
                 }
             }
+
             Plus => {
                 let n1 = stack.pop().expect("Couldn't pop from stack");
                 let n2 = stack.pop().expect("Couldn't pop from stack");
-                stack.push(n1 + n2);
+                match (n1, n2) {
+                    (Val::Float(a), Val::Float(b)) => stack.push(Val::Float(a + b)),
+                    _ => panic!("Couldn't add values, not all values were numbers."),
+                }
             }
+
+            Equal => {
+                let n1 = stack.pop().expect("Couldn't pop from stack");
+                let n2 = stack.pop().expect("Couldn't pop from stack");
+                match (n1, n2) {
+                    (Val::Float(a), Val::Float(b)) => {
+                        if a == b {
+                            stack.push(Val::Bool(true))
+                        } else {
+                            stack.push(Val::Bool(false))
+                        }
+                    }
+                    _ => panic!("Couldn't compare values, not all values were numbers."),
+                }
+            }
+
             Minus => {
                 let n1 = stack.pop().expect("Couldn't pop from stack");
                 let n2 = stack.pop().expect("Couldn't pop from stack");
-                stack.push(n1 - n2);
+                match (n1, n2) {
+                    (Val::Float(a), Val::Float(b)) => stack.push(Val::Float(a - b)),
+                    _ => panic!("Couldn't subtract values, not all values were numbers."),
+                }
             }
+
             Multiply => {
                 let n1 = stack.pop().expect("Couldn't pop from stack");
                 let n2 = stack.pop().expect("Couldn't pop from stack");
-                stack.push(n1 * n2);
+                match (n1, n2) {
+                    (Val::Float(a), Val::Float(b)) => stack.push(Val::Float(a * b)),
+                    _ => panic!("Couldn't multiply values, not all values were numbers."),
+                }
             }
+
             Divide => {
                 let n1 = stack.pop().expect("Couldn't pop from stack");
                 let n2 = stack.pop().expect("Couldn't pop from stack");
-                stack.push(n1 / n2);
+                match (n1, n2) {
+                    (Val::Float(a), Val::Float(b)) => stack.push(Val::Float(a / b)),
+                    _ => panic!("Couldn't divide values, not all values were numbers."),
+                }
+            }
+
+            Branch => {
+                let idx = stack.pop().expect("Couldn't pop from stack");
+                match idx {
+                    Val::Float(a) => return Some(a as usize),
+                    _ => panic!("Couldn't get index to stack location, wrong type."),
+                }
+            }
+            Cond => {
+                let idx = stack.pop().expect("Couldn't pop from stack");
+                let comparison = stack.pop().expect("Couldn't pop from stack");
+                match (idx, comparison) {
+                    (Val::Float(i), Val::Bool(cmp)) => {
+                        if cmp {
+                            return Some(i as usize);
+                        }
+                    }
+                    _ => panic!("Wrong type in comparison or index."),
+                }
             }
         }
+        None
     }
 }
 
@@ -97,15 +161,24 @@ fn parse(lexemes: Lexemes) -> AST {
     AST { tokens: v }
 }
 
-fn eval(ast: AST, stack: &mut Vec<f64>) {
+fn eval(ast: AST, stack: &mut Vec<Val>, debug: bool) {
     use Token::*;
 
-    for node in &ast.tokens {
-        match node {
-            Word(_) => continue,
-            Number(n) => stack.push(*n),
-            Builtin(func) => func.eval(stack),
+    let mut i = 0;
+    while i < ast.tokens.len() {
+        match &ast.tokens[i] {
+            Word(_) => (),
+            Number(n) => stack.push(Val::Float(*n)),
+            Builtin(func) => {
+                if let Some(idx) = func.eval(stack) {
+                    i = idx;
+                }
+            }
         }
+        if debug {
+            println!("{:?}", stack);
+        }
+        i += 1;
     }
 }
 
@@ -114,6 +187,8 @@ fn main() -> io::Result<()> {
 
     let mut stack = Vec::new();
 
+    let debugging = true;
+
     loop {
         buffer.clear();
         io::stdin().read_line(&mut buffer)?;
@@ -121,7 +196,7 @@ fn main() -> io::Result<()> {
 
         let lexemes = lex(&buffer);
         let ast = parse(lexemes);
-        eval(ast, &mut stack);
+        eval(ast, &mut stack, debugging);
 
         io::stdout().flush().unwrap();
 
