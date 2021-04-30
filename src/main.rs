748e88ea667fc57290fc62525c43553ec865d596
Syver Storm-Furru <syver.storm-furru@tv2.no>
2021-04-30 09:09:57 +0200

    Fix warnings related to unhandled results

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ use crossterm::{
     execute, terminal, Result,
 };
 
-fn main() -> io::Result<()> {
+fn main() -> Result<()> {
     let mut buffer = String::new();
 
     let mut stack = Vec::new();
@@ -30,7 +30,7 @@ fn main() -> io::Result<()> {
             run(&line, &mut stack, &mut words, false);
         }
     } else {
-        repl(&mut buffer, &mut stack, &mut words);
+        repl(&mut buffer, &mut stack, &mut words)?;
     }
     Ok(())
 }
@@ -124,7 +124,7 @@ fn repl(buffer: &mut String, stack: &mut Vec<A>, words: &mut HashMap<String, Ast
 
 fn print_char_in_place(buffer: &mut String, c: Option<char>) {
     let _ = buffer.pop();
-    execute!(
+    let _ = execute!(
         io::stdout(),
         cursor::MoveLeft(1),
         terminal::Clear(terminal::ClearType::UntilNewLine)
