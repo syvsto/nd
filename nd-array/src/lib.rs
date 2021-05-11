40ce9afa200ef5c635891e5c27068cfef9d826a3
Syver Storm-Furru <syver.storm-furru@tv2.no>
2021-05-11 08:42:57 +0200

    Use different typed representations for each array size, which avoids type errors and simplifies implementation of things like indexing

--- nd-array/src/lib.rs
+++ nd-array/src/lib.rs
@@ -1,46 +1,81 @@
 use std::ops::Index;
 use std::marker::Sized;
 
-pub enum R {
-    R0,
-    R1(usize),
-    R2([usize; 2]),
-    R3([usize; 3]),
+/// Row-major n-dimensional array
+#[derive(Debug)]
+pub struct A3<T>
+  where T: Sized + Clone {
+    shape: [usize; 3],
+    data: Vec<T>,
+}
+impl<T> A3<T>
+  where T: Sized + Clone {
+    pub fn new(shape: [usize; 3], data: &[T]) -> Self {
+        Self {
+            shape, data: data.to_vec()
+        }
+    }
+}
+impl<T: Sized + Clone> Index<[usize; 3]> for A3<T> {
+    type Output = T;
+
+    fn index(&self, idx: [usize; 3]) -> &Self::Output {
+        &self.data[self.shape[1] * self.shape[0] * idx[2] + self.shape[0] * idx[1] + idx[0]]
+    }
 }
 
 /// Row-major n-dimensional array
 #[derive(Debug)]
-pub struct A<T>
+pub struct A2<T>
   where T: Sized + Clone {
-    shape: Vec<usize>,
+    shape: [usize; 2],
     data: Vec<T>,
 }
-
-impl<T> A<T>
+impl<T> A2<T>
   where T: Sized + Clone {
-    pub fn new(shape: &[usize], data: &[T]) -> Self {
+    pub fn new(shape: [usize; 2], data: &[T]) -> Self {
         Self {
-            shape: shape.to_vec(), data: data.to_vec()
+            shape, data: data.to_vec()
         }
     }
 }
-
-impl<T: Sized + Clone> Index<R> for A<T> {
+impl<T: Sized + Clone> Index<[usize; 2]> for A2<T> {
     type Output = T;
 
-    fn index(&self, idx: R) -> &Self::Output {
-        use R::*;
-
-        let i = match idx {
-            R0 => 0,
-            R1(x) => x,
-            R2([x, y]) => (self.shape[0] * y + x),
-            R3([x, y, z]) => ((self.shape[0] * self.shape[1]) * z + (self.shape[0] * y + x)),
-        };
-
-        &self.data[i]
+    fn index(&self, idx: [usize; 2]) -> &Self::Output {
+        &self.data[self.shape[0] * idx[1] + idx[0]]
     }
 }
+
+/// Row-major n-dimensional array
+#[derive(Debug)]
+pub struct A1<T>
+  where T: Sized + Clone {
+    shape: [usize; 1],
+    data: Vec<T>,
+}
+impl<T> A1<T>
+  where T: Sized + Clone {
+    pub fn new(shape: usize, data: &[T]) -> Self {
+        Self {
+            shape: [shape], data: data.to_vec()
+        }
+    }
+}
+impl<T: Sized + Clone> Index<usize> for A1<T> {
+    type Output = T;
+
+    fn index(&self, idx: usize) -> &Self::Output {
+        &self.data[idx]
+    }
+}
+
+// impl<T: Sized + Clone> Iterator for A<T> {
+//     type Output = T;
+//     fn next(&mut self) -> Option<Self::Item> {
+        
+//     }
+// }
 
 #[macro_export]
 macro_rules! a {
@@ -50,23 +85,24 @@ macro_rules! a {
         let y = &v[0].len();
         let z = &v.len();
         let c: Vec<_> = v.into_iter().flatten().flatten().collect();
-        A::new(&[*x, *y, *z], &c.as_ref())
+        A3::new([*x, *y, *z], &c.as_ref())
     }};
     ([$([$($e:expr),*]),+]) => {{
         let v = vec![$(vec![$($e,)*],)*];
         let x = &v[0].len();
         let y = &v.len();
         let c: Vec<_> = v.into_iter().flatten().collect();
-        A::new(&[*x, *y], &c.as_ref())
+        A2::new([*x, *y], &c.as_ref())
     }};
     ([$($e:expr),*]) => {{
         let v = vec![$($e,)*];
-        A::new(&[v.len()], &v.as_ref())
+        A1::new(v.len(), &v.as_ref())
     }};
     ($e:expr) => {{
-      A::new(&[], &[$e])   
+      A0::new(&[$e])   
     }};
 }
+
 
 #[cfg(test)]
 mod tests {
@@ -74,19 +110,15 @@ mod tests {
 
     #[test]
     fn index() {
-        use R::*;
-        let a = a!(1);
         let b = a!([1, 2]);
         let c = a!([[1, 2], [3, 4]]);
         let d = a!([[[1, 2, 3], [3, 4, 5]], [[5, 6, 7], [7, 8, 9]]]);
 
-        let ix0 = R0;
-        let ix1 = R1(1);
-        let ix2 = R2([1, 0]);
-        let ix4 = R3([2, 1, 0]);
-        assert!(a[ix0] == 1);
-        assert!(b[ix1] == 2);
-        assert!(c[ix2] == 2);
-        assert!(d[ix4] == 5);
+        let ix0 = 1;
+        let ix1 = [1, 0];
+        let ix2 = [2, 1, 0];
+        assert!(b[ix0] == 2);
+        assert!(c[ix1] == 2);
+        assert!(d[ix2] == 5);
     }
 }
